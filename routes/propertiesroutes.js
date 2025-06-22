e3a7bc274874e11171ca11c7cfbf490a8fc7106b
velinaaas <vlnary221@gmail.com>
2025-06-22 12:52:08 +0700

    perbaikan route update properti

--- routes/propertiesroutes.js
+++ routes/propertiesroutes.js
@@ -4,10 +4,11 @@ const propertyController = require('../controllers/propertycontroller');
 const { verifyToken } = require('../middlewares/authmiddleware');
 const { verifyHost, verifyPropertyOwner } = require('../middlewares/authmiddleware');
 const upload = require('../middlewares/uploadCloudinary');
+const multer = require('multer');
 
 router.post('/add', verifyToken, upload.array('photos', 5), propertyController.createProperty);
 router.get('/get-host', verifyToken, propertyController.getHostProperties);
-router.put('/update/:id',verifyToken,verifyPropertyOwner,propertyController.updateProperty);
+router.put('/update/:id',verifyToken,verifyPropertyOwner,upload.any(), propertyController.updateProperty);
 router.delete('/delete/:id',verifyToken,verifyHost,verifyPropertyOwner,propertyController.deleteHostProperty);
 
 module.exports = router;
